906d78df9f6362a9aeb419977e138fc8c5ac79f4
SayamAlt <sayamk565@gmail.com>
2023-09-22 22:41:49 +0530

    Streamlit for model deployment

--- app.py
+++ app.py
@@ -1,114 +1,59 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[5]:
 
 
-from flask import Flask, render_template, request
+import streamlit as st
 import joblib
-from nltk.stem import PorterStemmer, WordNetLemmatizer
-from symspellpy import Verbosity, SymSpell
-from helper_prabowo_ml import remove_, remove_digits, remove_special_characters, remove_links, removeStopWords, punct, clean_html, email_address, non_ascii, lower
+from helper_prabowo_ml import clean_html, remove_special_characters, punct, lower, email_address, remove_digits, remove_links, remove_, removeStopWords, non_ascii
 
 
 # In[2]:
 
 
-app = Flask(__name__)
+model = joblib.load('backup_model.pkl')
 
 
 # In[3]:
 
 
-model = joblib.load('backup_model.pkl')
-model
-
-
-# In[4]:
-
-
-tfidf_vectorizer = joblib.load('tfidf_vectorizer.pkl')
-tfidf_vectorizer
-
-
-# In[5]:
-
-
-@app.route("/")
-def home():
-    return render_template('index.html')
+vectorizer = joblib.load('tfidf_vectorizer.pkl')
 
 
 # In[6]:
 
 
-stemmer = PorterStemmer()
-
-def stem_words(text):
-     return ' '.join(stemmer.stem(word) for word in text.split())
+label_mapping = {0: 'Books', 1: 'Clothing & Accessories', 2: 'Electronics', 3: 'Household'}
 
 
 # In[7]:
 
 
-lemmatizer = WordNetLemmatizer()
-
-def lemmatize_words(text):
-    return ' '.join(lemmatizer.lemmatize(word) for word in text.split())
+def main():
+    st.title('E-Commerce Category Prediction')
+    prod_description = st.text_area(label="Give details of your product:")
+    prod_description = lower(prod_description)
+    prod_description = punct(prod_description)
+    prod_description = email_address(prod_description)
+    prod_description = non_ascii(prod_description)
+    prod_description = remove_(prod_description)
+    prod_description = remove_digits(prod_description)
+    prod_description = remove_links(prod_description)
+    prod_description = remove_special_characters(prod_description)
+    prod_description = removeStopWords(prod_description)
+    prod_description = clean_html(prod_description)
+    prod_description = vectorizer.transform([prod_description])
+    
+    if st.button("Predict"):
+        pred = model.predict(prod_description)[0]
+        output = str(label_mapping[pred])
+        st.success(f"The category of your e-commerce product is {output.lower()}.")
 
 
 # In[8]:
 
 
-spelling_corrector = SymSpell()
-
-def correct_spellings(text):
-    corrected_tokens = []
-    
-    for token in text.split():
-        x = spelling_corrector.lookup(token,Verbosity.CLOSEST,max_edit_distance=2,include_unknown=True)[0].__str__()
-        y = x.split(',')[0]
-        corrected_tokens.append(y)
-    
-    return ' '.join(corrected_tokens)
-
-
-# In[9]:
-
-
-label_mapping = {0: 'Books', 1: 'Clothing & Accessories', 2: 'Electronics', 3: 'Household'}
-label_mapping
-
-
-# In[10]:
-
-
-@app.route("/predict",methods=['GET','POST'])
-def predict():
-    if request.method == 'POST':
-        prod_description = request.form['product_description']
-        prod_description = lower(prod_description)
-        prod_description = remove_(prod_description)
-        prod_description = remove_digits(prod_description)
-        prod_description = remove_links(prod_description)
-        prod_description = remove_special_characters(prod_description)
-        prod_description = removeStopWords(prod_description)
-        prod_description = punct(prod_description)
-        prod_description = non_ascii(prod_description)
-        prod_description = email_address(prod_description)
-        prod_description = clean_html(prod_description)
-        prod_description = stem_words(prod_description)
-        prod_description = lemmatize_words(prod_description)
-        prod_description = correct_spellings(prod_description)
-        vectorized_prod_description = tfidf_vectorizer.transform([prod_description])
-        pred = model.predict(vectorized_prod_description)[0]
-        output = str(label_mapping[pred]).lower()
-        return render_template('index.html',prediction_text=f'The e-commerce category based on specified product description is {output}.')
-
-
-# In[ ]:
-
-
-if __name__ == '__main__':
-    app.run(port=8080)
+if __name__ == "__main__":
+    main()
 
